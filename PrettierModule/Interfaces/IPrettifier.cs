1161049090af99d37b57765fd357273b6b670646
Vsevolod <sevoslav@gmail.com>
2022-12-08 23:36:12 -0500

    doc: Add sumary for the IPrettifier

--- PrettierModule/Interfaces/IPrettifier.cs
+++ PrettierModule/Interfaces/IPrettifier.cs
@@ -8,7 +8,21 @@ namespace PrettifierModule.Interfaces
 {
 	public interface IPrettifier
 	{
+		/// <summary>
+		/// Will prettify number. The prettified version should include one number after the decimal when the truncated number is not an integer. 
+		/// It will prettify numbers greater than 6 digits.
+		/// </summary>
+		/// <param name="number">Big number for prettified.</param>
+		/// <param name="accuracy">Accuracy of fractional part of prettified number. Default: 1.</param>
+		/// <returns>Prettified number. E.g. 3.5B, 1M etc.</returns>
 		string Prettify(string number, int accuracy = 1);
+		/// <summary>
+		/// Will prettify number. The prettified version should include one number after the decimal when the truncated number is not an integer. 
+		/// It will prettify numbers greater than 6 digits.
+		/// </summary>
+		/// <param name="number">Big number for prettified.</param>
+		/// <param name="accuracy">Accuracy of fractional part of prettified number. Default: 1.</param>
+		/// <returns>Prettified number. E.g. 3.5B, 1M etc.</returns>
 		string Prettify(double number, int accuracy = 1);
 	}
 }
